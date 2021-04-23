06ec10e0ca22dfd91a2859eae603234ce1224996
NguyenPhanNhatTu <18110232@student.hcmute.edu.vn>
2021-04-23 21:17:31 +0700

    [fix] fix login

---
+++ src/app/Controllers/KhachHangController.js
@@ -0,0 +1,38 @@
+const KhachHang = require('../models/KhachHang');
+class KhachHangController {
+  //GET KhachHang/login
+  async login(req, res, next) {
+    const { Email, Password } = req.body;
+    const result = await KhachHang.findOne({ Email, Password })
+    if (result != null) {
+      res.status(200).send(result);
+    } else {
+      res.status(400).send({
+        'error': 'Login that bai!',
+      });
+    }
+  }
+  //Post customers/register
+  async register(req, res, next) {
+    // res.json(req.body);
+    const formData = req.body;
+    try {
+      const result = await KhachHang.create(formData);
+      if (result != null) {
+        res.status(200).send(result);
+      } else {
+        res.status(400).send({
+          'error': 'Dang ky that bai',
+        });
+      }
+    }
+    catch
+    {
+      res.status(400).send({
+        'error': 'Dang ky that bai',
+      });
+    }
+  }
+}
+
+module.exports = new KhachHangController();
---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const app = express();
+const db= require('./config/db');
+const port = process.env.PORT || 8000;
+const route = require('./routes');
+
+
+db.connect();
+app.use(express.json());
+route(app);
+app.listen(port, () => {
+    console.log(`App listening at http://localhost`,port);
+  });
+  
