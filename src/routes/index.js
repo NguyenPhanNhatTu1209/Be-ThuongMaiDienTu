1aacf3cba14a140414df318ddf64e8d3c6242e2b
NguyenPhanNhatTu <18110232@student.hcmute.edu.vn>
2021-04-27 14:45:20 +0700

    [Tu:controller] done MeController

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const customersRouter = require('./customers');
 const enterprisesRouter  = require('./enterprises');
 const accountsRouter  = require('./accounts');
+const meRouter  = require('./me');
 function route(app) {
   app.use('/customers', customersRouter);
   app.use('/enterprises', enterprisesRouter);
   app.use('/auth',accountsRouter);
+  app.use('/me',meRouter);
 }
 module.exports = route;
