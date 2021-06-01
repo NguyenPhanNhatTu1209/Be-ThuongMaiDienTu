b2bd78ac106d7d2e1502b26ca35cc3a8b965e8d2
NguyenPhanNhatTu <18110232@student.hcmute.edu.vn>
2021-06-01 20:41:09 +0700

    [Tu:config] config aws ec2

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
-
-# Give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ubuntu/file/ec-2021
-sudo chmod -R 777 /home/ubuntu/file/ec-2021/dist
-
 # Navigate into our working directory where we have all our github files
 cd /home/ubuntu/file/ec-2021
 
