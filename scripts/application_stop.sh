66c32c192f927235a784af10c2c9d3ae3935f472
NguyenPhanNhatTu <18110232@student.hcmute.edu.vn>
2021-06-01 21:02:37 +0700

    [Tu:fix] fix aws

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,3 +1,3 @@
-# # Remove docker container ec-b010
-# sudo docker rm -f ec-b010
-# sudo docker rmi -f lambiengcode/ec-b010:latest
+# Remove docker container ec-b010
+sudo docker rm -f ec-b010
+sudo docker rmi -f lambiengcode/ec-b010:latest
